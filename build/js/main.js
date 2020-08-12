72759928b60f26da5ece23f86c6993fa225ec8d9
Hudson8811 <hudson88@yandex.ru>
2020-08-12 16:01:09 +0300

    add

--- build/js/main.js
+++ build/js/main.js
@@ -155,41 +155,109 @@ function initLessFullHdHeaderHandlers() {
             hMainMenu.removeClass('header-main-menu--tablet-active');
         }, 150);
     });
-    /*
-        $('.hmm-item__a:not(.js-open-sub-menu)').on('mouseenter.lessFullHdHeader', function () {
-            subMenuTriggers.removeClass('hmm-item__a--active');
+    $('.header,.header-main, .hmm-sub-menu').on('mouseout.lessFullHdHeader', function (event) {
+        if ($(event.relatedTarget).is('main.main') || $(event.relatedTarget).closest('main.main').length) {
+            shBurgerWrap.removeClass('sh-burger-wrap--active-sub').removeClass('sh-burger-wrap--active');
+            shBurger.removeClass('sh-burger--back').removeClass('sh-burger--cross');
             subMenus.removeClass('hmm-sub-menu--is-open');
-        });*/
+            setTimeout(function () {
+                hMainMenu.removeClass('header-main-menu--open-sub');
+                hMainMenu.removeClass('header-main-menu--tablet-active');
+            }, 150);
+            return;
+        }
+    });
 
-    /*  $('.js-open-sub-menu').on('mouseenter.fullHdHeader hover.fullHdHeader', function () {
-          subMenus.removeClass('hmm-sub-menu--is-open');
-          $(this).addClass('hmm-item__a--active').siblings('.hmm-sub-menu').addClass('hmm-sub-menu--is-open');
-      });
+    $(document).on('click.lessFullHdHeader', function (e) {
+        if ($(e.target).closest('.header').length) {
+            // клик внутри элемента
+            return;
+        }
+        // клик снаружи элемента
+        shBurgerWrap.removeClass('sh-burger-wrap--active-sub').removeClass('sh-burger-wrap--active');
+        shBurger.removeClass('sh-burger--back').removeClass('sh-burger--cross');
+        subMenus.removeClass('hmm-sub-menu--is-open');
+        setTimeout(function () {
+            hMainMenu.removeClass('header-main-menu--open-sub');
+            hMainMenu.removeClass('header-main-menu--tablet-active');
+        }, 150);
+    });
 
-      $('.hmm-item__a:not(.js-open-sub-menu)').on('mouseenter.fullHdHeader', function () {
-          subMenuTriggers.removeClass('hmm-item__a--active');
-          subMenus.removeClass('hmm-sub-menu--is-open');
-      });
-      */
-    /*  $(document).on('click.fullHdHeader', function (e) {
-          if ($(e.target).closest('.hmm-item--has-sub').length) {
-              // клик внутри элемента
-              return;
-          }
-          // клик снаружи элемента
-          subMenuTriggers.removeClass('hmm-item__a--active');
-          subMenus.removeClass('hmm-sub-menu--is-open');
-      });
-      $('.header-main, .hmm-sub-menu').on('mouseout.fullHdHeader', function (event) {
-          if ($(event.relatedTarget).is('main.main') || $(event.relatedTarget).closest('main.main').length) {
-              subMenuTriggers.removeClass('hmm-item__a--active');
-              subMenus.removeClass('hmm-sub-menu--is-open');
-              return;
-          }
-      });*/
 }
 
+function disableLessFullHdHeaderHandlers() {
+    $('.sh-burger-wrap').off('.lessFullHdHeader');
+    $('.js-open-sub-menu').off('.lessFullHdHeader');
+    $('.header,.header-main, .hmm-sub-menu').off('.lessFullHdHeader');
+    $('document').off('.lessFullHdHeader');
+}
 
+////////////////////////////////////////
+
+
+function initMobileHeaderHandlers() {
+    $('.sh-burger-wrap').on('click.mobileHeader', function (e) {
+        $this = $(this);
+        if ($this.is('.sh-burger-wrap--active')) {
+            if ($this.is('.sh-burger-wrap--active-sub')) {
+                shBurgerWrap.removeClass('sh-burger-wrap--active-sub');
+                shBurger.removeClass('sh-burger--back').addClass('sh-burger--cross');
+                subMenus.removeClass('hmm-sub-menu--is-open');
+                setTimeout(function () {
+                    hMainMenu.removeClass('header-main-menu--open-sub');
+                    hMainMenu.addClass('header-main-menu--mobile-active');
+                }, 150);
+
+
+            } else {
+                shBurgerWrap.removeClass('sh-burger-wrap--active');
+                shBurger.removeClass('sh-burger--cross');
+                hMainMenu.removeClass('header-main-menu--mobile-active');
+
+            }
+        } else {
+            $this.addClass('sh-burger-wrap--active')
+                .find('.sh-burger').addClass('sh-burger--cross');
+            hMainMenu.addClass('header-main-menu--mobile-active');
+        }
+    });
+
+    $('.js-open-sub-menu').on('click.mobileHeader', function () {
+        shBurger.addClass('sh-burger--back');
+        shBurgerWrap.addClass('sh-burger-wrap--active-sub');
+        subMenus.removeClass('hmm-sub-menu--is-open');
+        $(this).addClass('hmm-item__a--active').siblings('.hmm-sub-menu').addClass('hmm-sub-menu--is-open');
+        hMainMenu.addClass('header-main-menu--open-sub');
+    });
+  /*  $('.header,.header-main, .hmm-sub-menu').on('mouseout.mobileHeader', function (event) {
+        if ($(event.relatedTarget).is('main.main') || $(event.relatedTarget).closest('main.main').length) {
+            shBurgerWrap.removeClass('sh-burger-wrap--active-sub').removeClass('sh-burger-wrap--active');
+            shBurger.removeClass('sh-burger--back').removeClass('sh-burger--cross');
+            subMenus.removeClass('hmm-sub-menu--is-open');
+            setTimeout(function () {
+                hMainMenu.removeClass('header-main-menu--open-sub');
+                hMainMenu.removeClass('header-main-menu--mobile-active');
+            }, 150);
+            return;
+        }
+    });
+
+    $(document).on('click.mobileHeader', function (e) {
+        if ($(e.target).closest('.header').length) {
+            // клик внутри элемента
+            return;
+        }
+        // клик снаружи элемента
+        shBurgerWrap.removeClass('sh-burger-wrap--active-sub').removeClass('sh-burger-wrap--active');
+        shBurger.removeClass('sh-burger--back').removeClass('sh-burger--cross');
+        subMenus.removeClass('hmm-sub-menu--is-open');
+        setTimeout(function () {
+            hMainMenu.removeClass('header-main-menu--open-sub');
+            hMainMenu.removeClass('header-main-menu--mobile-active');
+        }, 150);
+    });
+*/
+}
 
 $(document).ready(function () {
     subMenus = $('.hmm-sub-menu');
@@ -197,6 +265,6 @@ $(document).ready(function () {
     shBurger = $('.sh-burger');
     shBurgerWrap = $('.sh-burger-wrap');
     hMainMenu = $('.header-main-menu');
-    initLessFullHdHeaderHandlers();
+    initMobileHeaderHandlers();
 
 });
