7e9288b157cb24119e1861ea40335d09bcda20c4
PipZip <zipperpipper@gmail.com>
2020-09-03 16:43:24 +0600

    temp

--- src/js/partials/doctors.js
+++ src/js/partials/doctors.js
@@ -10,4 +10,16 @@ $(document).ready(function () {
             $('.doctors-page__block').removeClass('hidden');
         }
     });
+
+    $('.serts-page__filter').on('click', function(){
+        $('.serts-page__filter').removeClass('active');
+        $(this).addClass('active');
+        var cat = $(this).data('cat');
+        if(cat !== 'all'){
+            $('.serts-page__block').addClass('hidden');
+            $('.serts-page__block[data-cat="'+cat+'"]').removeClass('hidden');
+        } else{
+            $('.serts-page__block').removeClass('hidden');
+        }
+    });
 });
